becd9debd43eabab8c9d22724ab9162d6405b436
ramirogomezfoto@outlook.com <ramirogomezfoto@outlook.com>
2019-09-28 12:17:54 -0300

    comit

---
+++ mindruner2/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mindruner2
+{
+    public partial class Form1 : Form
+    {
+        public string preguntas = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[,] preguntas = new string[,] { { "N", "0", "¿Cual es la relacion entre 3 integrantes del grupo de música Airbag?", "a. Primos.", "b. Hermanos.", "c. Amigos.", "b" } ,
+                {"N", "1", "¿Cual es el nombre del Hit del grupo de música Los Tipitos?", "a. Hechizo.", "b. Gualicho.", "c. Brujeria.", "c" } ,
+                {"N", "2", "¿Que instrumento tocaba Louis Armstrong?", "a. El piano.","b. El saxo.", "c. La trompeta.", "c" } ,
+                {"N", "3", "¿Cual es el nombre artístico de Bob Zimmerman?", "a. Bob Marley.", "b. Bob Dylan.", "c. Bob Hope.", "b" },
+                {"N", "4", "¿Cuantas cuerdas tiene el violonchelo?", "a. Cuatro.", "b. Seis.", "c. Ocho.", "a", },
+                {"N", "5", "6. ¿Quién escribió Caperucita Roja?", "a. Auguste Perret.", "b. Charles Perrault.", "c. Jean Perrin.", "b" },
+
+                {"A", "0", "¿Que personaje de Star Wars se convierte en Darth Vader?", "a. Skywalker.","b. Kenobi.","c. Yoda.", "a" },
+                {"A", "1", "¿Que animal es Patricio en la serie Bob Esponja?", "a. Delfín.", "b. Estrella de mar.", "c. Cangrejo.", "b" },
+                {"A", "2", "¿Que actor representa a Jack Sparrow en Piratas del Caribe?", "a. Orlando Bloom.", "b. Antonio Banderas.", "c. Johnny Deep.", "c" },
+                {"A", "3", "¿Que personaje de los Simpson tiene un bypass?", "a. El señor Burns.", "b. Moe.", "c. Krosty el payaso.", "c",  },
+                {"A", "4", "¿Como se llama el gato del brujo de los pitufos?","a. Asrael.", "b. Asmal.", "c. Gargamen.", "a" },
+                {"A", "5", "¿Que tipo de pez es Nemo?", "a. Pez globo.", "b. Pez dorado.", "c. Pez payaso.", "c" },
+                
+                {"M", "0", "¿Que alimento nunca se echa a perder?", "a. La miel", "b. El chocolate.", "c. La batata.", "a"},
+                {"M", "1", "¿Que le arrojó Susana Gimenez a Huberto Roviralta?", "a. Un cenicero.", "b. Un libro.", "c. Un florero.", "a"}, 
+                {"M", "2", "¿Como se llamaba la hermanita de Charlie Brown?", "a. Maggie.", "b. Sally.", "c. Lucy.", "b"}, 
+                {"M", "3", "¿Que clase de comida es una marquise?", "a. Un pastel de carne.", "b. Un postre.", "c. Un timbal de macarrones.", "b"}, 
+                {"M", "4", "¿En que década de inventó internet?", "a. En los años 60.", "b. En los años 70.", "c. En los años 80.", "a"  },
+                {"M", "5", "¿Que país tiene un machete en la bandera?", "a. Haití", "b. Filipinas.", "c. Angola.", "c"  },
+                
+                {"V", "0", "¿Que equipo tiene su estadio en el barrio de Nueva Italia en Córdoba?", "a. Deportivo Italiano", "b. Racing de Córdoba.", "c. Talleres.", "b"   },
+                {"V", "1", "¿A quienes les dicen lechuzas en el fútbol?", "a. Los arbitros.", "b. Los fotógrafos.", "c. Los hinchas mufa.", "b" },
+                {"V", "2", "¿A que equipo italiano se lo conoce como La Veccia Signora?", "a. Inter.", "b. Juventus.", "c. Sampdoria", "b" },
+                {"V", "3", "¿En que país se disputaron por primera vez los juegos panamericanos?", "a. Argentina.", "b. Brasil.", "c. Uruguay.", "b" },
+                {"V", "4", "¿En que deporte se ha destacado Shaquille O' Neal?", "a. Basquet.", "b. Boxeo.", "c. Natacion.", "a" },
+                {"V", "5", "¿Cuantos partidos de Copa del Mundo jugó Diego Armando Maradona?", "a. 12.", "b. 21.", "c. 35.", "b"},
+                
+                {"C", "0", "¿En que continente estan las montañas rocallosas?", "a. Asia.", "b. Europa.", "c. America.", "c" },
+                {"C", "1", "¿En que país está el Taj Mahal?.", "a. Arabia", "b. Tailandia.", "c. India.", "c"  },
+                {"C", "2", "¿Quienes dominaban Perú hasta la llegada de los españoles?", "a. Los Mayas", "b. Los Incas.", "c. Los Aztecas.", "b"  },
+                {"C", "3", "¿En que provincia está la ciudad de Trelew?", "a. Rio Negro.", "b. Chubut.", "c. Santa Cruz.", "b"  },
+                {"C", "4", "¿En que país está la región de Transilvania, patria de Drácula?", "a. Rumania.", "b. Alemania.","c. Rusia.", "a"  },
+                {"C", "5", "¿En que país es el quechua una lengua oficial?", "a. Peru.","b. Paraguay.", "c. Bolivia.", "a" }
+                
+                {"R", "0", "¿Cuantos nanosegundos hay en un segundo?","a. Cien millones.", "b. Mil millones.", "c. Diez mil millones.", "b" } ,
+                {"R", "1", "¿Quien inventó la radio?", "a. Graham Bell","b. Marconi.", "c. Edison.", "b"},
+                {"R", "2", "Una piara es un conjunto ¿de que?","a. De peces.","b. De cerdos.","c. De perros.", "b" },
+                {"R", "3", "¿Cual es el planeta más denso del sistema solar?", "a. Plutón.","b. Mercurio.","c. La Tierra.", "c" }.
+                {"R", "4", "¿Que animal es la araña de mar?", "a.La ostra.", "b. La langosta.", "c. La centolla.", "c" },
+                {"R", "5", "¿Que estudia un herpetólogo?", "a. Reptiles.","b. Cavernas.","C. Herpes.", "a" } };
+
+
+
+
+        }
+    }
+
+    }
+    
+
