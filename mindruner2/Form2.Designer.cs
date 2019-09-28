a610bc234fb82996638ceb6ecddde24244ac5d23
Maxi Cappelletti <kape2254@gmail.com>
2019-09-28 18:00:18 -0300

    agregando la clase para los dados

--- mindruner2/Form2.Designer.cs
+++ mindruner2/Form2.Designer.cs
@@ -28,28 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.pbdado2 = new System.Windows.Forms.PictureBox();
+            this.pbdado1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbdado2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbdado1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Location = new System.Drawing.Point(55, 71);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(141, 121);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(405, 71);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(141, 121);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
@@ -63,6 +47,24 @@
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
+            // pbdado2
+            // 
+            this.pbdado2.Location = new System.Drawing.Point(397, 71);
+            this.pbdado2.Name = "pbdado2";
+            this.pbdado2.Size = new System.Drawing.Size(141, 121);
+            this.pbdado2.TabIndex = 2;
+            this.pbdado2.TabStop = false;
+            // 
+            // pbdado1
+            // 
+            this.pbdado1.Image = global::mindruner2.Properties.Resources.lado1;
+            this.pbdado1.Location = new System.Drawing.Point(70, 71);
+            this.pbdado1.Name = "pbdado1";
+            this.pbdado1.Size = new System.Drawing.Size(141, 121);
+            this.pbdado1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbdado1.TabIndex = 1;
+            this.pbdado1.TabStop = false;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -70,20 +72,20 @@
             this.BackColor = System.Drawing.Color.MediumSpringGreen;
             this.ClientSize = new System.Drawing.Size(619, 450);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pbdado2);
+            this.Controls.Add(this.pbdado1);
             this.Name = "Form2";
             this.Text = "Form2";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbdado2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbdado1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pbdado1;
+        private System.Windows.Forms.PictureBox pbdado2;
         private System.Windows.Forms.Button button1;
     }
 }
---
+++ mindruner2/dado.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mindruner2
+{
+   public class dado
+    {
+        private int numero;
+
+        public void lanzar (Random r)
+        {
+            numero = r.Next(6) + 1;
+
+        }
+
+        public int numeroobtenido()
+        {
+            return numero;
+
+        }
+
+        public void mostrar(System.Windows.Forms.PictureBox pb )
+
+        {
+            switch (numero)
+            {
+                case 1:
+
+                    pb.Image = mindruner2.Properties.Resources.lado1;
+                    break;
+
+                case 2:
+
+                    pb.Image = mindruner2.Properties.Resources.lado2;
+                    break;
+
+                case 3:
+
+                    pb.Image = mindruner2.Properties.Resources.lado3;
+                    break;
+
+                case 4:
+
+                    pb.Image = mindruner2.Properties.Resources.lado4;
+                    break;
+
+                case 5:
+
+                    pb.Image = mindruner2.Properties.Resources.lado5;
+                    break;
+
+                case 6:
+
+                    pb.Image = mindruner2.Properties.Resources.lado6;
+                    break;
+            }
+
+        }
+    }
+}
