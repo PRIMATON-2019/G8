ffb25205c0b8209dc93e51747f2bb161f210f77e
Maxi Cappelletti <kape2254@gmail.com>
2019-10-03 15:16:46 -0300

    dados funcionando ok

--- mindruner2/Form2.Designer.cs
+++ mindruner2/Form2.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.btnlanzar = new System.Windows.Forms.Button();
-            this.pb = new System.Windows.Forms.PictureBox();
             this.lbljugador1 = new System.Windows.Forms.Label();
             this.lbljugador2 = new System.Windows.Forms.Label();
             this.lbljugador3 = new System.Windows.Forms.Label();
@@ -38,7 +37,6 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -49,112 +47,92 @@
             // 
             this.btnlanzar.BackColor = System.Drawing.Color.Fuchsia;
             this.btnlanzar.Font = new System.Drawing.Font("Comic Sans MS", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnlanzar.Location = new System.Drawing.Point(383, 89);
-            this.btnlanzar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnlanzar.Location = new System.Drawing.Point(287, 72);
             this.btnlanzar.Name = "btnlanzar";
-            this.btnlanzar.Size = new System.Drawing.Size(108, 75);
+            this.btnlanzar.Size = new System.Drawing.Size(81, 61);
             this.btnlanzar.TabIndex = 3;
             this.btnlanzar.Text = "d";
             this.btnlanzar.UseVisualStyleBackColor = false;
             this.btnlanzar.Click += new System.EventHandler(this.Button1_Click);
             // 
-            // pb
-            // 
-            this.pb.Image = global::mindruner2.Properties.Resources.lado1;
-            this.pb.Location = new System.Drawing.Point(605, 36);
-            this.pb.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.pb.Name = "pb";
-            this.pb.Size = new System.Drawing.Size(188, 149);
-            this.pb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pb.TabIndex = 1;
-            this.pb.TabStop = false;
-            // 
             // lbljugador1
             // 
             this.lbljugador1.AutoSize = true;
-            this.lbljugador1.Location = new System.Drawing.Point(16, 36);
-            this.lbljugador1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbljugador1.Location = new System.Drawing.Point(12, 29);
             this.lbljugador1.Name = "lbljugador1";
-            this.lbljugador1.Size = new System.Drawing.Size(46, 17);
+            this.lbljugador1.Size = new System.Drawing.Size(35, 13);
             this.lbljugador1.TabIndex = 4;
             this.lbljugador1.Text = "label1";
             // 
             // lbljugador2
             // 
             this.lbljugador2.AutoSize = true;
-            this.lbljugador2.Location = new System.Drawing.Point(16, 121);
-            this.lbljugador2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbljugador2.Location = new System.Drawing.Point(12, 98);
             this.lbljugador2.Name = "lbljugador2";
-            this.lbljugador2.Size = new System.Drawing.Size(46, 17);
+            this.lbljugador2.Size = new System.Drawing.Size(35, 13);
             this.lbljugador2.TabIndex = 5;
             this.lbljugador2.Text = "label2";
             // 
             // lbljugador3
             // 
             this.lbljugador3.AutoSize = true;
-            this.lbljugador3.Location = new System.Drawing.Point(16, 214);
-            this.lbljugador3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbljugador3.Location = new System.Drawing.Point(12, 174);
             this.lbljugador3.Name = "lbljugador3";
-            this.lbljugador3.Size = new System.Drawing.Size(46, 17);
+            this.lbljugador3.Size = new System.Drawing.Size(35, 13);
             this.lbljugador3.TabIndex = 6;
             this.lbljugador3.Text = "label3";
             // 
             // lbljugador4
             // 
             this.lbljugador4.AutoSize = true;
-            this.lbljugador4.Location = new System.Drawing.Point(16, 309);
-            this.lbljugador4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbljugador4.Location = new System.Drawing.Point(12, 251);
             this.lbljugador4.Name = "lbljugador4";
-            this.lbljugador4.Size = new System.Drawing.Size(46, 17);
+            this.lbljugador4.Size = new System.Drawing.Size(35, 13);
             this.lbljugador4.TabIndex = 7;
             this.lbljugador4.Text = "label4";
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(88, 15);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Location = new System.Drawing.Point(66, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(60, 50);
+            this.pictureBox1.Size = new System.Drawing.Size(45, 41);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(88, 101);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Location = new System.Drawing.Point(66, 82);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(60, 50);
+            this.pictureBox2.Size = new System.Drawing.Size(45, 41);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 9;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(88, 196);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox3.Location = new System.Drawing.Point(66, 159);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(60, 50);
+            this.pictureBox3.Size = new System.Drawing.Size(45, 41);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 10;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox4
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(88, 290);
-            this.pictureBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox4.Location = new System.Drawing.Point(66, 236);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(60, 50);
+            this.pictureBox4.Size = new System.Drawing.Size(45, 41);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox4.TabIndex = 11;
             this.pictureBox4.TabStop = false;
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MediumSpringGreen;
-            this.ClientSize = new System.Drawing.Size(825, 554);
+            this.ClientSize = new System.Drawing.Size(619, 450);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
@@ -164,12 +142,8 @@
             this.Controls.Add(this.lbljugador2);
             this.Controls.Add(this.lbljugador1);
             this.Controls.Add(this.btnlanzar);
-            this.Controls.Add(this.pb);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form2";
             this.Text = "Form2";
-            this.Load += new System.EventHandler(this.Form2_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
@@ -180,8 +154,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pb;
         private System.Windows.Forms.Button btnlanzar;
         private System.Windows.Forms.Label lbljugador1;
         private System.Windows.Forms.Label lbljugador2;
--- mindruner2/Form2.cs
+++ mindruner2/Form2.cs
@@ -25,85 +25,193 @@ namespace mindruner2
         private void Button1_Click(object sender, EventArgs e)
         {
 
-            for (int h = 0; h < 4; h++)
-            {
+           
                 d1.lanzar();
                 d2.lanzar();
                 d3.lanzar();
                 d4.lanzar();
 
+            for (int i = 0; i < 4; i++)
+            {
+
+            
+
+            if (d1.numero == d2.numero)
+            {
+                d2.lanzar();
+
+            }
+                if (d2.numero == d3.numero)
+                {
+                d3.lanzar();
+                }
+
+            if (d1.numero == d3.numero)
+            {
+                d3.lanzar();
+
+            }
+            if (d4.numero == d1.numero)
+            {
+                d4.lanzar();
+
+            }
+            if (d4.numero == d2.numero)
+            {
+                d4.lanzar();
             }
 
-            for (int i = 0; i < 4; i++)
-            { 
-                switch (pepe[i])
+            if (d3.numero == d4.numero)
+            {
+                d4.lanzar();
+            }
+
+            }
+
+
+
+
+
+            switch (d1.numero)
                 {
                     case 1:
 
-                        pb.Image = mindruner2.Properties.Resources.lado1;
+                    pictureBox1.Image = mindruner2.Properties.Resources.lado1;
                         break;
 
                     case 2:
 
-                        pb.Image = mindruner2.Properties.Resources.lado2;
+                    pictureBox1.Image = mindruner2.Properties.Resources.lado2;
                         break;
 
                     case 3:
 
-                        pb.Image = mindruner2.Properties.Resources.lado3;
+                    pictureBox1.Image = mindruner2.Properties.Resources.lado3;
                         break;
 
                     case 4:
 
-                        pb.Image = mindruner2.Properties.Resources.lado4;
+                    pictureBox1.Image = mindruner2.Properties.Resources.lado4;
                         break;
 
                     case 5:
 
-                        pb.Image = mindruner2.Properties.Resources.lado5;
+                    pictureBox1.Image = mindruner2.Properties.Resources.lado5;
                         break;
 
                     case 6:
 
-                        pb.Image = mindruner2.Properties.Resources.lado6;
+                     pictureBox1.Image = mindruner2.Properties.Resources.lado6;
                         break;
                 }
 
-
-            switch (pepe.Length)
+            switch (d2.numero)
             {
                 case 1:
 
-                    pictureBox1.Image = pb.Image;
+                    pictureBox2.Image = mindruner2.Properties.Resources.lado1;
                     break;
-
 
                 case 2:
 
-                    pictureBox2.Image = pb.Image;
-
+                    pictureBox2.Image = mindruner2.Properties.Resources.lado2;
                     break;
 
                 case 3:
 
-                    pictureBox3.Image = pb.Image;
-
+                    pictureBox2.Image = mindruner2.Properties.Resources.lado3;
                     break;
 
                 case 4:
 
-                    pictureBox4.Image = pb.Image;
+                    pictureBox2.Image = mindruner2.Properties.Resources.lado4;
+                    break;
 
+                case 5:
+
+                    pictureBox2.Image = mindruner2.Properties.Resources.lado5;
+                    break;
+
+                case 6:
+
+                    pictureBox2.Image = mindruner2.Properties.Resources.lado6;
                     break;
             }
+
+            switch (d3.numero)
+            {
+                case 1:
+
+                    pictureBox3.Image = mindruner2.Properties.Resources.lado1;
+                    break;
+
+                case 2:
+
+                    pictureBox3.Image = mindruner2.Properties.Resources.lado2;
+                    break;
+
+                case 3:
+
+                    pictureBox3.Image = mindruner2.Properties.Resources.lado3;
+                    break;
+
+                case 4:
+
+                    pictureBox3.Image = mindruner2.Properties.Resources.lado4;
+                    break;
+
+                case 5:
+
+                    pictureBox3.Image = mindruner2.Properties.Resources.lado5;
+                    break;
+
+                case 6:
+
+                    pictureBox3.Image = mindruner2.Properties.Resources.lado6;
+                    break;
+            }
+
+            switch (d4.numero)
+            {
+                case 1:
+
+                    pictureBox4.Image = mindruner2.Properties.Resources.lado1;
+                    break;
+
+                case 2:
+
+                    pictureBox4.Image = mindruner2.Properties.Resources.lado2;
+                    break;
+
+                case 3:
+
+                    pictureBox4.Image = mindruner2.Properties.Resources.lado3;
+                    break;
+
+                case 4:
+
+                    pictureBox4.Image = mindruner2.Properties.Resources.lado4;
+                    break;
+
+                case 5:
+
+                    pictureBox4.Image = mindruner2.Properties.Resources.lado5;
+                    break;
+
+                case 6:
+
+                    pictureBox4.Image = mindruner2.Properties.Resources.lado6;
+                    break;
+            }
+          
         }
+            
+        }
+
     }
 
 
-        private void Form2_Load(object sender, EventArgs e)
-        {
+        
+    
 
-        }
-    }
-}
 
