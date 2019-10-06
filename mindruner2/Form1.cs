a79835fb8cee25ea5b2640ea4af9d38083e1147d
mariano ramos <marianoso1@hotmail.com>
2019-10-06 19:05:20 -0300

    Add Instrucction and Option

--- mindruner2/Form1.cs
+++ mindruner2/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace mindruner2
 {
@@ -16,6 +17,9 @@ namespace mindruner2
         public Form1()
         {
             InitializeComponent();
+            SoundPlayer Player = new SoundPlayer();
+            Player.SoundLocation = "C:/Users/maria/source/repos/G8-master/mindruner2/Sound/Final_fantasy_xi_ost_rulude_gardens (Inicio).wav";
+            Player.Play();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -32,6 +36,23 @@ namespace mindruner2
         {
             Form tercerformulario = new Form3jugadores ();
             tercerformulario.Show();
+        }
+
+        private void BtnInstructions_Click(object sender, EventArgs e)
+        {
+            Form Instrucciones = new Formulario_Instrucciones();
+            Instrucciones.Show();
+        }
+
+        private void BtnOptions_Click(object sender, EventArgs e)
+        {
+            Form FormularioOpciones = new Formulario_Opciones();
+            FormularioOpciones.Show();
+        }
+
+        private void BtnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 
---
+++ mindruner2/Formulario Instrucciones.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mindruner2
+{
+    public partial class Formulario_Instrucciones : Form
+    {
+        public Formulario_Instrucciones()
+        {
+            InitializeComponent();
+        }
+
+        private void Formulario_Instrucciones_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile("C:/Users/maria/source/repos/G8-master/mindruner2/imagenes/Garfield.gif");
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            pictureBox2.Image = Image.FromFile("C:/Users/maria/source/repos/G8-master/mindruner2/imagenes/snoppy.gif");
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+    
+}
