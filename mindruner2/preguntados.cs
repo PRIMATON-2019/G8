da268d34e76ddba4b9c647e65cbd47485a6526a7
elcesarok <54405055+elcesarok@users.noreply.github.com>
2019-10-10 00:56:10 -0300

    cargué las matrices de historia(preguntados) y Geografía(preguntasgeografia)

--- mindruner2/preguntados.cs
+++ mindruner2/preguntados.cs
@@ -9,21 +9,33 @@ namespace mindruner2
     public class preguntados
     {
         
-        private string[,] PYR = new string[6, 5];
+        private string[,] PYR = new string[20, 5];
 
         
         #region Metodos
 
         public string Preguntas(string NumerodePregunta)
         {
-            PYR[0, 0] = "quien descubrio america";             
-            PYR[1, 0] = "quien es el chabo del 8"; 
-            PYR[2, 0] = "quien cruzo los andes";
-            PYR[3, 0] = "quien es el chabo del 8";
-            PYR[4, 0] = "que sale de mezclar amarillo y azul";
-            PYR[4, 4] = "2";
-            PYR[5, 0] = "quien es super man";
-            PYR[5, 4] = "1";
+            PYR[0, 0] = "¿En que ciudad europea está el Palacio de la UNESCO?";             
+            PYR[1, 0] = "¿Quién Mató, según el historiador romano Marcelino, al invencible Atila?"; 
+            PYR[2, 0] = "¿Cuántos eran los Caballeros de la Mesa Redonda?";
+            PYR[3, 0] = "¿Qué país inventó los barriltes y los tallarines";
+            PYR[4, 0] = "¿Dónde murió el General Jósé Gervasio Artigas?";
+            PYR[5, 0] = "¿Qué ciudad uruguaya se lamó originariamente Pueblo Ituzaingó?";
+            PYR[6, 0] =  "¿Cuál es el seudónimo del mexicano Doroteo Arango?";
+            PYR[7, 0] =  "¿Que nombre es la W de George W. Bush?";
+            PYR[8, 0] =  "¿Quienes dominaban Perú hasta la llegada de los españoles?";
+            PYR[9, 0] =  "¿En qué país nació Sor Juana Inés de la Cruz?";
+            PYR[10, 0] =  "¿En qué ciudad italiana nació Galileo Galilei?";
+            PYR[11, 0] =  "¿En 1893 una país fue el primero en otorgar el voto femenino?";
+            PYR[12, 0] =  "¿A qué oficio se dedicaba José el marido de la virgen María?";
+            PYR[13, 0] =  "¿Qué nombre corresponde a la B del destacado polílitico socialista argentino Juan B. Justo?";
+            PYR[14, 0] =  "¿En qué país nació la Madre Teresa de Calcuta?";
+            PYR[15, 0] =  "¿A quién asesinó Mark David Chapman?";
+            PYR[16, 0] =  "¿Quién dijo (Vine, ví, Vencí)";
+            PYR[17, 0] =  "¿En qué país fue presidente Getúlio Vargas?";
+            PYR[18, 0] =  "¿En qué cermenterio está enterrado el General Perón?";
+            PYR[19, 0] =  "¿Cómo se llamaba la esposa de Luis XVI?";
                        
             string pregunta = PYR[Convert.ToInt32(NumerodePregunta) - 1, 0];
                  
@@ -32,24 +44,52 @@ namespace mindruner2
         }
         public string opc1(string NumerodePregunta)
         {
-            PYR[0, 1] = " Napoleon";
-            PYR[1, 1] = " kiko";
-            PYR[2, 1] = " san martin";
-            PYR[3, 1] = " kiko";
-            PYR[4, 1] = " violeta";
-            PYR[5, 1] = " clar ken";
+            PYR[0, 1] = " En Ginebra";
+            PYR[1, 1] = " Los Godos";
+            PYR[2, 1] = " Veinte";
+            PYR[3, 1] = " Los Chinos";
+            PYR[4, 1] = " Argentina";
+            PYR[5, 1] = " Maldonado";
+            PYR[6, 1] =  "Zapata";
+            PYR[7, 1] =  "Willian";
+            PYR[8, 1] =  "Los Mayas";
+            PYR[9, 1] =  "España";
+            PYR[10, 1] =  "Pisa";
+            PYR[11, 1] =  "Irlanda";
+            PYR[12, 1] =  "Era Herrero";
+            PYR[13, 1] =  "Bernardo";
+            PYR[14, 1] =  "En Albania";
+            PYR[15, 1] =  "A Martín Luther King";
+            PYR[16, 1] =  "Napoléon";
+            PYR[17, 1] =  "Bolivia";
+            PYR[18, 1] =  "La Recoleta";
+            PYR[19, 1] =  "Ana Bolena";
             string opc1 = PYR[Convert.ToInt32(NumerodePregunta) - 1, 1];
             return opc1;
 
         }
         public string opc2(string NumerodePregunta)
         {
-            PYR[0, 2] = "Jesus";
-            PYR[1, 2] = "el chapulin";
-            PYR[2, 2] = "pepe argento";
-            PYR[3, 2] = "el chapulin";
-            PYR[4, 2] = "verde";
-            PYR[5, 2] = "barby";
+            PYR[0, 2] = "En París";
+            PYR[1, 2] = "Los ostrogodos";
+            PYR[2, 2] = "Doce";
+            PYR[3, 2] = "Los Egipcios";
+            PYR[4, 2] = "Uruguay";
+            PYR[5, 2] = "Punta del Este";
+            PYR[6, 2] =  "Pancho Villa";
+            PYR[7, 2] =  "Walter";
+            PYR[8, 2] =  "Los Incas";
+            PYR[9, 2] =  "Chile";
+            PYR[10, 2] =  "Roma";
+            PYR[11, 2] =  "Francia";
+            PYR[12, 2] =  "Era Carpintero";
+            PYR[13, 2] =  "Bautista";
+            PYR[14, 2] =  "En India";
+            PYR[15, 2] =  "A.J.F. Kennedy";
+            PYR[16, 2] =  "Julio César";
+            PYR[17, 2] =  "Venezuela";
+            PYR[18, 2] =  "Chacarita";
+            PYR[19, 2] =  "Margarita de Navarra";
 
 
             string opc2 = PYR[Convert.ToInt32(NumerodePregunta) - 1, 2];
@@ -57,31 +97,57 @@ namespace mindruner2
         }
         public string opc3(string NumerodePregunta)
         {
-            PYR[0, 3] = "Colon";
-            PYR[1, 3] = "chespirito";
-            PYR[2, 3] = "tinelli";
-            PYR[3, 3] = "chespirito";
-            PYR[4, 3] = "negro";
-            PYR[5, 3] = "chapo guzma";
+            PYR[0, 3] = "En Madrid";
+            PYR[1, 3] = "Su última Esposa";
+            PYR[2, 3] = "Diez";
+            PYR[3, 3] = "Los griegos";
+            PYR[4, 3] = "Paraguay";
+            PYR[5, 3] = "Colonia";
+            PYR[6, 3] =  "Cantinflas";
+            PYR[7, 3] =  "Walker";
+            PYR[8, 3] =  "Los Aztecas";
+            PYR[9, 3] =  "Mexico";
+            PYR[10, 3] =  "Florencia";
+            PYR[11, 3] =  "Nueva Zelanda";
+            PYR[12, 3] =  "Era pastor";
+            PYR[13, 3] =  "Blas";
+            PYR[14, 3] =  "En la ex Yugoslavia";
+            PYR[15, 3] =  "A Jhon Lenon";
+            PYR[16, 3] =  "Carlos V";
+            PYR[17, 3] =  "Brasil";
+            PYR[18, 3] =  "Olivos";
+            PYR[19, 3] =  "María Antonieta";
 
             string opc3 = PYR[Convert.ToInt32(NumerodePregunta) - 1, 3];
             return opc3;
         }
         public string resultado (string NumerodePregunta)
         {
-            PYR[0, 4] = "3";
+            PYR[0, 4] = "2";
             PYR[1, 4] = "3";
-            PYR[2, 4] = "1";
-            PYR[3, 4] = "3";
-            PYR[4, 4] = "2";
-            PYR[5, 4] = "1";
+            PYR[2, 4] = "2";
+            PYR[3, 4] = "1";
+            PYR[4, 4] = "3";
+            PYR[5, 4] = "2";
+            PYR[6, 4] =  "2";
+            PYR[7, 4] =  "3";
+            PYR[8, 4] =  "2";
+            PYR[9, 4] =  "3";
+            PYR[10, 4] =  "2";
+            PYR[11, 4] =  "3";
+            PYR[12, 4] =  "2";
+            PYR[13, 4] =  "2";
+            PYR[14, 4] =  "1";
+            PYR[15, 4] =  "3";
+            PYR[16, 4] =  "2";
+            PYR[17, 4] =  "3";
+            PYR[18, 4] =  "2";
+            PYR[19, 4] =  "3";
             string resultado = PYR[Convert.ToInt32(NumerodePregunta) - 1, 4];
             return resultado;
         }
         #endregion
-
-
-        //mm
+              
     }
 
 }
--- mindruner2/preguntasgeografia.cs
+++ mindruner2/preguntasgeografia.cs
@@ -8,20 +8,34 @@ namespace mindruner2
 {
     class preguntasgeografia
     {
-        private string[,] PYR = new string[6, 5];
+        private string[,] PYR = new string[20, 5];
 
 
         #region Metodos
 
         public string Preguntas(string NumerodePregunta)
         {
-            PYR[0, 0] = "quien hizo rambo";
-            PYR[1, 0] = "como es el apellido de susana";
-            PYR[2, 0] = "quien conduce Show Mach";
-            PYR[3, 0] = "quien es el actor principal de el sexto sentido";
-            PYR[4, 0] = "cuanto personages tiene la serie FRIENDS";
-            PYR[5, 0] = "Como se llama el hijo de HOMERO";
-
+            PYR[0, 0] = "¿En que continente estan las montañas Rocallosas?";
+            PYR[1, 0] = "¿En que país está el Taj Mahal?.";
+            PYR[2, 0] =  "¿En que provincia está la ciudad de Trelew?";
+            PYR[3, 0] =  "¿En que país está la región de Transilvania, patria de Drácula?"o";
+            PYR[4, 0] =  "¿En que país es el quechua una lengua oficial?";
+            PYR[5, 0] =  "¿Que país tiene un machete en la bandera?" ;
+            PYR[6, 0] =  "¿De que provincia es la capital de la ciudad de Paraná?";
+            PYR[7, 0] =  " ¿En que país se encuentra la ciudad de Viña del Mar?";
+            PYR[8, 0] =  "¿Cerca de las costas de que país está la Isla de Aruba?";
+            PYR[9, 0] =  "¿En que país está el puerto de Casablaca?";
+            PYR[10, 0] =  "¿En que mar se encuentra la Isla de Chipre?";
+            PYR[11, 0] =  "¿Cómo se dice al que nació en Vizcaya?";
+            PYR[12, 0] =  "¿En que mar desemboca el Río Nilo?";
+            PYR[13, 0] =  "¿En que ciudad estadounidense está el puente Golden Gate?";
+            PYR[14, 0] =  "Hélade era el nombre primitivo ¿de que país?";
+            PYR[15, 0] =  "¿A que país pertenece la Isla Margarita";
+            PYR[16, 0] =  "¿Que animal hay en la bandera de bután";
+            PYR[17, 0] =  "¿Cuál es el único país africano en donde se habla español?";
+            PYR[18, 0] =  "¿Cuál es el país del mundo que tiene más volcanes?";
+            PYR[19, 0] =  "¿En que mar desemboca el Río Jordán?";
+ 
 
             string pregunta = PYR[Convert.ToInt32(NumerodePregunta) - 1, 0];
 
@@ -30,24 +44,52 @@ namespace mindruner2
         }
         public string opc1(string NumerodePregunta)
         {
-            PYR[0, 1] = " chuck Norris";
-            PYR[1, 1] = " gonzalez";
-            PYR[2, 1] = " Mirtha Legrand";
-            PYR[3, 1] = " Bruce Willis";
-            PYR[4, 1] = " 7";
-            PYR[5, 1] = " lisa";
+            PYR[0, 1] = " Asia.";
+            PYR[1, 1] = " Arabia";
+            PYR[2, 1] = "  Rio Negro";
+            PYR[3, 1] = "  Rumania";
+            PYR[4, 1] = " Peru ";
+            PYR[5, 1] = " Haití";
+            PYR[6, 1] =  " Entre Ríos";
+            PYR[7, 1] =  "España";
+            PYR[8, 1] =  "Colombia";
+            PYR[9, 1] =  "Uruguay";
+            PYR[10, 1] =  "en el Mediterráneo";
+            PYR[11, 1] =  "Vizcaíno";
+            PYR[12, 1] =  "Rojo";
+            PYR[13, 1] =  "Los Angeles";
+            PYR[14, 1] =  "Grecia";
+            PYR[15, 1] =  "Cuba";
+            PYR[16, 1] =  "Un Águila";
+            PYR[17, 1] =  "Mauritania";
+            PYR[18, 1] =  "Estados Unidos";
+            PYR[19, 1] =  "En el Mar Caspio";
             string opc1 = PYR[Convert.ToInt32(NumerodePregunta) - 1, 1];
             return opc1;
 
         }
         public string opc2(string NumerodePregunta)
         {
-            PYR[0, 2] = "Bruce wils";
-            PYR[1, 2] = "Gimenez";
-            PYR[2, 2] = "Marcelo Tinelli";
-            PYR[3, 2] = "el chapulin";
-            PYR[4, 2] = "20";
-            PYR[5, 2] = "bart";
+            PYR[0, 2] =" Europa.";
+            PYR[1, 2] = "Tailandia";
+            PYR[2, 2] = "Chubut";
+            PYR[3, 2] = " Alemania";
+            PYR[4, 2] = "Paraguay";
+            PYR[5, 2] = "Filipinas ";
+            PYR[6, 2] =  "Santa Fe ";
+            PYR[7, 2] =  "Chile";
+            PYR[8, 2] =  "Brasil";
+            PYR[9, 2] =  "México";
+            PYR[10, 2] =  "En el Mar de Oman";
+            PYR[11, 2] =  "Vizcaitarra ";
+            PYR[12, 2] =  " Mediterraneo";
+            PYR[13, 2] =  "San Francisco";
+            PYR[14, 2] =  "Islandia";
+            PYR[15, 2] =  "Ecuador";
+            PYR[16, 2] =  "Un León";
+            PYR[17, 2] =  "Sierra Leona";
+            PYR[18, 2] =  "Indonesia";
+            PYR[19, 2] =  "En el Mar Muerto";
 
 
             string opc2 = PYR[Convert.ToInt32(NumerodePregunta) - 1, 2];
@@ -55,12 +97,26 @@ namespace mindruner2
         }
         public string opc3(string NumerodePregunta)
         {
-            PYR[0, 3] = "Silvestre Stalone";
-            PYR[1, 3] = "Perez";
-            PYR[2, 3] = "Charly Garcia";
-            PYR[3, 3] = "Maradona";
-            PYR[4, 3] = "5";
-            PYR[5, 3] = "maguie";
+            PYR[0, 3] = " America.";
+            PYR[1, 3] = "India";
+            PYR[2, 3] = "Santa Cruz";
+            PYR[3, 3] = " Rusia";
+            PYR[4, 3] = " Bolivia";
+            PYR[5, 3] = "Angola";
+            PYR[6, 3] =  "Misiones ";
+            PYR[7, 3] =  "Colombia";
+            PYR[8, 3] =  "Venezuela";
+            PYR[9, 3] =  "Marruecos";
+            PYR[10, 3] =  "En el Mar Negro";
+            PYR[11, 3] =  "Vizcalino";
+            PYR[12, 3] =  "Arábigo";
+            PYR[13, 3] =  "Houston";
+            PYR[14, 3] =  "Finlandia";
+            PYR[15, 3] =  "Venezuela";
+            PYR[16, 3] =  "Un Dragón";
+            PYR[17, 3] =  "Guinea Ecuatorial";
+            PYR[18, 3] =  "Japón";
+            PYR[19, 3] =  "En el Mar Mediterraneo";
 
             string opc3 = PYR[Convert.ToInt32(NumerodePregunta) - 1, 3];
             return opc3;
@@ -68,11 +124,26 @@ namespace mindruner2
         public string resultado(string NumerodePregunta)
         {
             PYR[0, 4] = "3";
-            PYR[1, 4] = "2";
+            PYR[1, 4] = "3";
             PYR[2, 4] = "2";
             PYR[3, 4] = "1";
-            PYR[4, 4] = "3";
-            PYR[5, 4] = "2";
+            PYR[4, 4] = "1";
+            PYR[5, 4] = "3";
+            PYR[6, 4] =  "1";
+            PYR[7, 4] =  "2";
+            PYR[8, 4] =  "3";
+            PYR[9, 4] =  "3";
+            PYR[10, 4] =  "1";
+            PYR[11, 4] =  "1";
+            PYR[12, 4] =  "2";
+            PYR[13, 4] =  "2";
+            PYR[14, 4] =  "1";
+            PYR[15, 4] =  "3";
+            PYR[16, 4] =  "3";
+            PYR[17, 4] =  "3";
+            PYR[18, 4] =  "2";
+            PYR[19, 4] =  "2";
+
             string resultado = PYR[Convert.ToInt32(NumerodePregunta) - 1, 4];
             return resultado;
         }
