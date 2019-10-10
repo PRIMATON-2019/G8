2b96b6a396a4d1a5a981d26f16c043450b0ba0ce
Maxi Cappelletti <kape2254@gmail.com>
2019-10-09 23:59:15 -0300

    matrices listas...completar preguntas y respuestas faltantes!!!

--- mindruner2/preguntascine.cs
+++ mindruner2/preguntascine.cs
@@ -8,7 +8,7 @@ namespace mindruner2
 {
     class preguntascine
     {
-        private string[,] PYR = new string[6, 5];
+        private string[,] PYR = new string[20, 5];
 
 
         #region Metodos
@@ -21,7 +21,24 @@ namespace mindruner2
             PYR[3, 0] = "¿Que tipo de pez es Nemo?";
             PYR[4, 0] = "¿Que animal es Patricio en la serie Bob Esponja? ";           
             PYR[5, 0] = "¿Cual es la pelicula más taquillera de la historia?";
+            PYR[6, 0] = "¿?";
+            PYR[7, 0] = "¿?";
+            PYR[8, 0] = "¿?";
+            PYR[9, 0] = "¿?";
+            PYR[10, 0] = "¿ ";
+            PYR[11, 0] = "¿Cual?";
+            PYR[12, 0] = "¿Qu?";
+            PYR[13, 0] = "¿Que ae?";
+            PYR[14, 0] = "¿Ques?";
+            PYR[15, 0] = "¿Que ?";
+            PYR[16, 0] = "¿Qja? ";
+            PYR[17, 0] = "¿C?";
+            PYR[18, 0] = "¿Q?";
+            PYR[19, 0] = "¿Que a?";
+        
             
+
+
 
             string pregunta = PYR[Convert.ToInt32(NumerodePregunta) - 1, 0];
 
@@ -36,6 +53,22 @@ namespace mindruner2
             PYR[3, 1] = " Pez payaso.";
             PYR[4, 1] = " Delfín";
             PYR[5, 1] = " Titanic";
+            PYR[6, 1] = " ";
+            PYR[7, 1] = " ";
+            PYR[8, 1] = " ";
+            PYR[9, 1] = " .";
+            PYR[10, 1] = " ";
+            PYR[11, 1] = " ";
+            PYR[12, 1] = " ";
+            PYR[13, 1] = " ";
+            PYR[14, 1] = " ";
+            PYR[15, 1] = " .";
+            PYR[16, 1] = " ";
+            PYR[17, 1] = " ";
+            PYR[18, 1] = " ";
+            PYR[19, 1] = " ";
+     
+
             string opc1 = PYR[Convert.ToInt32(NumerodePregunta) - 1, 1];
             return opc1;
 
@@ -48,6 +81,21 @@ namespace mindruner2
             PYR[3, 2] = "Pez globo";
             PYR[4, 2] = "Cangrejo";
             PYR[5, 2] = "Avengers: Eng Game";
+            PYR[6, 2] = " ";
+            PYR[7, 2] = " ";
+            PYR[8, 2] = " ";
+            PYR[9, 2] = " .";
+            PYR[10, 2] = " ";
+            PYR[11, 2] = " ";
+            PYR[12, 2] = " ";
+            PYR[13, 2] = " ";
+            PYR[14, 2] = " ";
+            PYR[15, 2] = " .";
+            PYR[16, 2] = " ";
+            PYR[17, 2] = " ";
+            PYR[18, 2] = " ";
+            PYR[19, 2] = " ";
+
 
 
             string opc2 = PYR[Convert.ToInt32(NumerodePregunta) - 1, 2];
@@ -61,6 +109,21 @@ namespace mindruner2
             PYR[3, 3] = "Pez dorado";
             PYR[4, 3] = "Estrella de mar rosa";
             PYR[5, 3] = "Avatar";
+            PYR[6, 3] = " ";
+            PYR[7, 3] = " ";
+            PYR[8, 3] = " ";
+            PYR[9, 3] = " .";
+            PYR[10, 3] = " ";
+            PYR[11, 3] = " ";
+            PYR[12, 3] = " ";
+            PYR[13, 3] = " ";
+            PYR[14, 3] = " ";
+            PYR[15, 3] = " .";
+            PYR[16, 3] = " ";
+            PYR[17, 3] = " ";
+            PYR[18, 3] = " ";
+            PYR[19, 3] = " ";
+
 
             string opc3 = PYR[Convert.ToInt32(NumerodePregunta) - 1, 3];
             return opc3;
@@ -73,6 +136,22 @@ namespace mindruner2
             PYR[3, 4] = "1";
             PYR[4, 4] = "3";
             PYR[5, 4] = "2";
+            PYR[6, 4] = "";
+            PYR[7, 4] = "";
+            PYR[8, 4] = "";
+            PYR[9, 4] = "";
+            PYR[10, 4] = "";
+            PYR[11, 4] = "";
+            PYR[12, 4] = "";
+            PYR[13, 4] = "";
+            PYR[14, 4] = "";
+            PYR[15, 4] = "";
+            PYR[16, 4] = "";
+            PYR[17, 4] = "";
+            PYR[18, 4] = "";
+            PYR[19, 4] = "";
+      
+
             string resultado = PYR[Convert.ToInt32(NumerodePregunta) - 1, 4];
             return resultado;
         }
--- mindruner2/preguntasnaturaleza.cs
+++ mindruner2/preguntasnaturaleza.cs
@@ -21,7 +21,20 @@ namespace mindruner2
             PYR[3, 0] = "quien es el actor principal de el sexto sentido";
             PYR[4, 0] = "cuanto personages tiene la serie FRIENDS";
             PYR[5, 0] = "Como se llama el hijo de HOMERO";
-
+            PYR[6, 0] = "¿?";
+            PYR[7, 0] = "¿?";
+            PYR[8, 0] = "¿?";
+            PYR[9, 0] = "¿?";
+            PYR[10, 0] = "¿ ";
+            PYR[11, 0] = "¿Cual?";
+            PYR[12, 0] = "¿Qu?";
+            PYR[13, 0] = "¿Que ae?";
+            PYR[14, 0] = "¿Ques?";
+            PYR[15, 0] = "¿Que ?";
+            PYR[16, 0] = "¿Qja? ";
+            PYR[17, 0] = "¿C?";
+            PYR[18, 0] = "¿Q?";
+            PYR[19, 0] = "¿Que a?";
 
             string pregunta = PYR[Convert.ToInt32(NumerodePregunta) - 1, 0];
 
@@ -36,6 +49,21 @@ namespace mindruner2
             PYR[3, 1] = " Bruce Willis";
             PYR[4, 1] = " 7";
             PYR[5, 1] = " lisa";
+            PYR[6, 1] = " ";
+            PYR[7, 1] = " ";
+            PYR[8, 1] = " ";
+            PYR[9, 1] = " .";
+            PYR[10, 1] = " ";
+            PYR[11, 1] = " ";
+            PYR[12, 1] = " ";
+            PYR[13, 1] = " ";
+            PYR[14, 1] = " ";
+            PYR[15, 1] = " .";
+            PYR[16, 1] = " ";
+            PYR[17, 1] = " ";
+            PYR[18, 1] = " ";
+            PYR[19, 1] = " ";
+
             string opc1 = PYR[Convert.ToInt32(NumerodePregunta) - 1, 1];
             return opc1;
 
@@ -48,6 +76,21 @@ namespace mindruner2
             PYR[3, 2] = "el chapulin";
             PYR[4, 2] = "20";
             PYR[5, 2] = "bart";
+            PYR[6, 2] = " ";
+            PYR[7, 2] = " ";
+            PYR[8, 2] = " ";
+            PYR[9, 2] = " .";
+            PYR[10, 2] = " ";
+            PYR[11, 2] = " ";
+            PYR[12, 2] = " ";
+            PYR[13, 2] = " ";
+            PYR[14, 2] = " ";
+            PYR[15, 2] = " .";
+            PYR[16, 2] = " ";
+            PYR[17, 2] = " ";
+            PYR[18, 2] = " ";
+            PYR[19, 2] = " ";
+
 
 
             string opc2 = PYR[Convert.ToInt32(NumerodePregunta) - 1, 2];
@@ -61,6 +104,20 @@ namespace mindruner2
             PYR[3, 3] = "Maradona";
             PYR[4, 3] = "5";
             PYR[5, 3] = "maguie";
+            PYR[6, 3] = " ";
+            PYR[7, 3] = " ";
+            PYR[8, 3] = " ";
+            PYR[9, 3] = " .";
+            PYR[10, 3] = " ";
+            PYR[11, 3] = " ";
+            PYR[12, 3] = " ";
+            PYR[13, 3] = " ";
+            PYR[14, 3] = " ";
+            PYR[15, 3] = " .";
+            PYR[16, 3] = " ";
+            PYR[17, 3] = " ";
+            PYR[18, 3] = " ";
+            PYR[19, 3] = " ";
 
             string opc3 = PYR[Convert.ToInt32(NumerodePregunta) - 1, 3];
             return opc3;
@@ -73,6 +130,20 @@ namespace mindruner2
             PYR[3, 4] = "1";
             PYR[4, 4] = "3";
             PYR[5, 4] = "2";
+            PYR[6, 4] = "";
+            PYR[7, 4] = "";
+            PYR[8, 4] = "";
+            PYR[9, 4] = "";
+            PYR[10, 4] = "";
+            PYR[11, 4] = "";
+            PYR[12, 4] = "";
+            PYR[13, 4] = "";
+            PYR[14, 4] = "";
+            PYR[15, 4] = "";
+            PYR[16, 4] = "";
+            PYR[17, 4] = "";
+            PYR[18, 4] = "";
+            PYR[19, 4] = "";
             string resultado = PYR[Convert.ToInt32(NumerodePregunta) - 1, 4];
             return resultado;
         }
